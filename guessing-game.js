a3e002ce2f6a895cbbe9e0aa3651edfacde3eba5
Chalon Lubin <chalonlubin1@gmail.com>
2023-02-17 12:41:44 -0800

    completed guessing-game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,25 @@
 function guessingGame() {
+  let count = 0;
+  let ranNum = Math.floor(Math.random() * 100);
+  let won = false;
 
+  return function inner(x) {
+    if (won) return "The game is over, you already won!";
+    count += 1;
+
+    if (ranNum === x) {
+      won = true;
+      return `You win! You found ${ranNum} in ${count} guesses.`;
+    } else {
+      return x > ranNum ? `${x} is too high!` : `${x} is too low!`;
+    }
+  };
 }
 
+let game = guessingGame();
+
+game(10);
+game(20);
+game(25);
+game(30);
 module.exports = { guessingGame };
