d902cbebe4ac90e5e8085f00c04714f16c1b7c39
Chalon Lubin <chalonlubin1@gmail.com>
2023-02-24 12:55:03 -0800

    Add functionality to createAccount utilizing closure

--- create-account.js
+++ create-account.js
@@ -1,5 +1,44 @@
-function createAccount(pin, amount) {
+"use strict";
 
+function createAccount(pin, initialAmount) {
+  let amount = initialAmount;
+
+  // helper function to check pin
+  function checkPin(inputPin) {
+    if (inputPin !== pin) {
+      throw new Error("Invalid PIN.");
+    }
+  }
+
+  // utilize closure
+  return {
+    checkBalance(inputPin) {
+      checkPin(inputPin);
+      return amount;
+    },
+    deposit(inputPin, amt) {
+      checkPin(inputPin);
+      amount += amt;
+      return `Successfully deposited ${amt}, your new total is ${amount}`;
+    },
+    withdraw(inputPin, amt) {
+      checkPin(inputPin);
+      if (amount - amt >= 0) {
+        amount -= amt;
+        return `Successfully withdrew ${amt}, your new total is ${amount}`;
+      } else {
+        return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      }
+    },
+    changePin(inputPin, newPin) {
+      checkPin(inputPin);
+      pin = newPin;
+      return "PIN successfully changed!";
+    },
+  };
 }
+
+let account = createAccount("1234", 100);
+console.log(account.checkBalance("1234"));
 
 module.exports = { createAccount };
